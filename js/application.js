d86883434a45351fb5de13eccee9328da03a5fe2
tygern <tygern@gmail.com>
2014-01-11 16:07:12 -0700

    various tweaks

--- js/application.js
+++ js/application.js
@@ -1,40 +1,5 @@
 var hangryApp = angular.module('hangryApp', []);
 
-hangryApp.controller('PickerController', function ($scope, $http) {
-    $scope.choices = [];
-    $scope.tags = [];
-    $scope.count = 0;
-
-    Hangry.loadData($scope, $http, 'data/restaurants.json');
-
-    $scope.toggle = function toggle(item) {
-        if (item.selected) {
-            $scope.count -= 1;
-        } else {
-            $scope.count += 1;
-        }
-
-        item.selected = !item.selected;
-    };
-
-    $scope.clearTags = function clearTags() {
-        $.each($scope.tags, function (i, tag) {
-            tag.selected = false;
-        });
-        $scope.count = 0;
-    };
-
-    $scope.clearSearch = function clearSearch() {
-        $scope.query = null;
-    };
-
-    $scope.$watch('count', function () {
-        Hangry.updateScores($scope.tags, $scope.choices, $scope.count);
-        Hangry.setOpacities($scope.choices, $scope.count);
-    });
-
-});
-
 Hangry = {
     loadData: function loadData(target, fetcher, file) {
         var that = this;
@@ -90,7 +55,5 @@ Hangry = {
         return $.map(tags, function (tag) {
             return { "name": tag, "selected": false}
         });
-        debugger
     }
-
 };
---
+++ js/controller.js
@@ -0,0 +1,33 @@
+hangryApp.controller('PickerController', function ($scope, $http) {
+    $scope.choices = [];
+    $scope.tags = [];
+    $scope.count = 0;
+
+    Hangry.loadData($scope, $http, 'data/restaurants.json');
+
+    $scope.toggle = function toggle(item) {
+        if (item.selected) {
+            $scope.count -= 1;
+        } else {
+            $scope.count += 1;
+        }
+
+        item.selected = !item.selected;
+    };
+
+    $scope.clearTags = function clearTags() {
+        $.each($scope.tags, function (i, tag) {
+            tag.selected = false;
+        });
+        $scope.count = 0;
+    };
+
+    $scope.clearSearch = function clearSearch() {
+        $scope.query = null;
+    };
+
+    $scope.$watch('count', function () {
+        Hangry.updateScores($scope.tags, $scope.choices, $scope.count);
+        Hangry.setOpacities($scope.choices, $scope.count);
+    });
+});
