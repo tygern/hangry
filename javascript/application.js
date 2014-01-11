0f7c10d239206ffea9e5986fda5c14057f309329
tygern <tygern@gmail.com>
2014-01-11 10:42:03 -0700

    allow tags to be selected

--- javascript/application.js
+++ javascript/application.js
@@ -6,16 +6,30 @@ hangryApp.controller('PickerController', function ($scope, $http) {
 
     Hangry.loadData($scope, $http, 'data/restaurants.json');
 
+    $scope.toggle = function toggle(item) {
+        item.selected = !item.selected;
+    }
+
 });
 
 Hangry = {
     loadData: function loadData(target, fetcher, file) {
+        var that = this;
+
         fetcher.get(file)
             .then(function (response) {
                 target.choices = response.data;
                 $.each(target.choices, function (i, choice) {
                     $.unique($.merge(target.tags, choice.tags));
                 });
-            })
+                target.tags = that.bootstrapTags(target.tags)
+            });
+
+    },
+
+    bootstrapTags: function bootstrapTags(tags) {
+        return $.map(tags, function (tag) {
+            return { "name": tag, "selected": false}
+        });
     }
 };
