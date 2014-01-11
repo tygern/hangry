85248c0bcbab3235cc65d61b4af79dcb6739c1a0
tygern <tygern@gmail.com>
2014-01-11 16:06:29 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+/usr/local/bin/aws s3 cp ./data s3://tyson.gern.us/hangry/data --recursive --acl public-read
+/usr/local/bin/aws s3 cp ./fonts s3://tyson.gern.us/hangry/fonts --recursive --acl public-read
+/usr/local/bin/aws s3 cp ./js s3://tyson.gern.us/hangry/js --recursive --acl public-read
+/usr/local/bin/aws s3 cp ./style s3://tyson.gern.us/hangry/style --recursive --acl public-read
+/usr/local/bin/aws s3 cp ./index.html s3://tyson.gern.us/hangry/ --acl public-read
